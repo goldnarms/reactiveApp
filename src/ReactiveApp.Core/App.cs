02266833b6efdd4693fabccc63ca06cf181d1c2c
arnstein <arnstej@gmail.com>
2018-11-20 09:27:26 +0100

    Add MvvmCross template

---
+++ src/ReactiveApp.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using ReactiveApp.Core.ViewModels.Home;
+
+namespace ReactiveApp.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<HomeViewModel>();
+        }
+    }
+}
